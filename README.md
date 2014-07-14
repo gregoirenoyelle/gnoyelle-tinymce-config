6ea013ea077f878f2eadbec299e4f07c97770459
Grégoire Noyelle <hello@gregoirenoyelle.com>
2014-07-14 20:46:17 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TinyMCE 4 config
+
+* changer bouton order for default setup
+* remove address and code
+* remove custom color
---
+++ tinymce-config.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: gnoyelle : Config TinyMCE 4
+Plugin URI: http://wwww.gregoirenoyelle.com
+Description: Options pour l'éditeur TinyMCE 4
+Version: 1.0
+Author: Grégoire Noyelle
+Author URI: http://wwww.gregoirenoyelle.com
+License: GPL2
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    die;
+}
+
+/***
+ * default set up for $in['toolbar1']
+ * bold,italic,strikethrough,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,wp_fullscreen,wp_adv
+ * default set up for $in['toolbar2']
+ * formatselect,underline,alignjustify,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help
+ * default set up for $in['block_formats']
+ * Paragraph=p;Address=address;Pre=pre;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5; Heading 6=h6
+ */
+function gn_tinymce_filtre($in){
+    $in['keep_styles']=true;
+	// $in['paste_remove_styles']=true;
+ 	// $in['paste_remove_spans']=true;
+ 	$in['toolbar1']='formatselect,bold,italic,underline,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,alignjustify,link,unlink,wp_more,spellchecker,wp_fullscreen,wp_adv ';
+    $in['toolbar2']='pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help ';
+    $in['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5';
+    return $in;
+  }
+add_filter('tiny_mce_before_init', 'gn_tinymce_filtre');
+
+
