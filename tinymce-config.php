df003b3eb1fb260644bd12a7378a122634026969
Grégoire Noyelle <hello@gregoirenoyelle.com>
2014-07-14 20:52:00 +0200

    Add update and remove unused code

--- tinymce-config.php
+++ tinymce-config.php
@@ -7,6 +7,8 @@ Version: 1.0
 Author: Grégoire Noyelle
 Author URI: http://wwww.gregoirenoyelle.com
 License: GPL2
+GitHub Plugin URI: https://github.com/gregoirenoyelle/gnoyelle-tinymce-config
+GitHub Branch: master
 */
 
 // If this file is called directly, abort.
@@ -15,6 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /***
+ * DEFAULT SET UP
  * default set up for $in['toolbar1']
  * bold,italic,strikethrough,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,wp_fullscreen,wp_adv
  * default set up for $in['toolbar2']
@@ -23,9 +26,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Paragraph=p;Address=address;Pre=pre;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5; Heading 6=h6
  */
 function gn_tinymce_filtre($in){
-    $in['keep_styles']=true;
-	// $in['paste_remove_styles']=true;
- 	// $in['paste_remove_spans']=true;
  	$in['toolbar1']='formatselect,bold,italic,underline,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,alignjustify,link,unlink,wp_more,spellchecker,wp_fullscreen,wp_adv ';
     $in['toolbar2']='pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help ';
     $in['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5';
